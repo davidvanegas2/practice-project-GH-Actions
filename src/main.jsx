be75ff8f82b4eddabea1c919c0256f1f9f4c896a
David Felipe Vanegas Ramirez <david@loka.com>
2024-08-26 20:25:14 -0500

    ci: Demo to run

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 
 // Demo to doesn't run workflow
+// Demo to run workflow
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
